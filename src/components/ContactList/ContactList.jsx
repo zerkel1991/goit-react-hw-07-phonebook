47a2dadcfa70059d63c3399f8d4e0605c19e13c8
Vyacheslav <47851511+zerkel1991@users.noreply.github.com>
2022-09-04 13:37:32 +0400

    Fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,54 +1,60 @@
-import s from './ContactList.module.css'
-import {useGetContactsQuery,useDeleteContactMutation} from '../../redux/contactsApi';
+import s from './ContactList.module.css';
+import {
+  useGetContactsQuery,
+  useDeleteContactMutation,
+} from '../../redux/contactsApi';
 import { useSelector } from 'react-redux';
-
-
-
+import { useState } from 'react';
 
 function ContactList() {
-const filter = useSelector(state => state.filter);
-const {data, isLoading,isError} = useGetContactsQuery()
-const [deleteContact] = useDeleteContactMutation()
+  const [isDeleting, setIsDeleting] = useState(false);
+  const filter = useSelector(state => state.filter);
+  const { data, isLoading, isError } = useGetContactsQuery();
 
-const handleDeleteContact = async (id) => {
-  await deleteContact(id).unwrap()
-}
+  const [deleteContact] = useDeleteContactMutation();
 
-if (isLoading) {
-  return <h1>Загружаем контакты....</h1>
-}
-if(isError) {
-  return <h1>Упс, бекэнд сломался</h1>
-}
+  const handleDeleteContact = async id => {
+    setIsDeleting(true);
+    await deleteContact(id).unwrap();
+    setIsDeleting(false);
+  };
 
-const getFilteredContacts = () =>{
-    if(!data){
+  if (isLoading) {
+    return <h1>Загружаем контакты....</h1>;
+  }
+  if (isError) {
+    return <h1>Упс, бекэнд сломался</h1>;
+  }
 
+  const getFilteredContacts = () => {
+    if (!data) {
     }
-    
+
     const normalizedFilter = filter.toLowerCase();
 
     return data.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter));
-  }
-  const filteredData = getFilteredContacts()
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  const filteredData = getFilteredContacts();
 
-
- return (
-
-  <ul>
-  {filteredData && filteredData.map(el =>(
-   <li key = {el.id}>
-    <span className={s.span}>{el.name} : </span>
-    <span>{el.number}</span>
-    <button onClick={()=> handleDeleteContact(el.id)}>Удалить</button>
-   </li>
-  ))}
-</ul>
-
- )
-
+  return (
+    <ul>
+      {filteredData &&
+        filteredData.map(el => (
+          <li key={el.id}>
+            <span className={s.span}>{el.name} : </span>
+            <span>{el.number}</span>
+            <button
+              disabled={isDeleting}
+              onClick={() => handleDeleteContact(el.id)}
+            >
+              {isDeleting ? 'Удаляю...' : 'Удалить'}
+            </button>
+          </li>
+        ))}
+    </ul>
+  );
 }
 
-
-export default ContactList
+export default ContactList;
